dc2c842c5bbe4e917e9fa9cf271bb625b8e05ae6
Andy <amgregoi08@gmail.com>
2014-09-21 02:29:04 -0500

    added assets, finished homepage.  Still need to fix how we query the DB for word of the day.

--- Lexiday/src/com/android/lexiday/LoginActivity.java
+++ Lexiday/src/com/android/lexiday/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.ParseException;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.Menu;
@@ -14,16 +15,22 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 public class LoginActivity extends ActionBarActivity implements OnClickListener{
 
 	EditText uEmail, uPassword;
-	String token;
-	
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		
+		Parse.initialize(this, "EbTGZ0TfK4NhcGyX2X4nXS7rDbhPyZGGF4ZAha0M", "M6CfIKGVcYVHU1PrhQanrvuorIlyFXWJ3T92eku6");
+		setTitle("");
 		uEmail = (EditText)findViewById(R.id.user_email);
 		uPassword = (EditText)findViewById(R.id.user_password);
 	}
@@ -49,65 +56,123 @@ public class LoginActivity extends ActionBarActivity implements OnClickListener{
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()){
-		case R.id.button1:
-			//do sign in
-			onLoginClicked();
-			break;
-		case R.id.button2:
-			//do sign up
-			break;
-		default:
-			break;
+		if(!isNetworkOn(getBaseContext())) 
+	    {
+	        Toast.makeText(getBaseContext(), "No network connection", Toast.LENGTH_SHORT).show();
+	    }
+		else
+		{
+			switch(v.getId()){
+				case R.id.signIn:
+					//do sign in
+					onLoginClicked();
+					break;
+				case R.id.signUp:
+					onSignupClicked();
+					break;
+				default:
+					break;
+			}
 		}
 	}
 	
-	private void onLoginClicked() {
-	    if(!isNetworkOn(getBaseContext())) 
-	    {
-	        Toast.makeText(getBaseContext(), "No network connection", Toast.LENGTH_SHORT).show();
-	    } 
-	    else 
-	    {
-	    	if(isDataValid())
-	    	{
-	    		//sign in
-	    		token = "this is the token";
-	    		Intent i = new Intent(this, MainActivity.class).putExtra("Token", token);
-	    		startActivity(i);
-	    	}
-	    	else
-	    	{
-	    		//failed login - retry
-	    	}
-	    }
+	
+	/*
+	 * verifies user is already a user in parse.com
+	 * and signs them in
+	 */
+	private void onLoginClicked() throws ParseException {
+    	if(isDataValid())
+    	{    		
+    		ParseUser.logInInBackground(getEmail(), getPassword(), new LogInCallback() {
+				@Override
+				public void done(ParseUser user, com.parse.ParseException e) {
+					if (user != null) 
+					{
+    			    	startMainActivity();
+    			    } 
+					else {
+    			      // Signup failed. Look at the ParseException to see what happened.
+    	        		Toast.makeText(getBaseContext(), "Wrong username or password", Toast.LENGTH_SHORT).show();
+    			    	System.out.println(e.toString());
+    			    }
+				}
+    		});
+    	}
+    	
+
+	}
+	
+	/*
+	 * sign user up, and add them to parse.com database
+	 */
+	public void onSignupClicked(){
+    	if(isDataValid())
+    	{
+    		//sign up
+    		ParseUser user = new ParseUser();
+    		user.setUsername(getEmail());
+    		user.setPassword(getPassword());
+    		user.setEmail(getEmail());
+    		user.signUpInBackground(new SignUpCallback() {
+				@Override
+				public void done(com.parse.ParseException e) {
+					if (e == null) 
+				    {
+				    	startMainActivity();
+					}
+				    else 
+					{
+					  // Sign up didn't succeed. Look at the ParseException
+				    	System.out.println(e.toString());
+    	        		Toast.makeText(getBaseContext(), "Sign up failed", Toast.LENGTH_SHORT).show();
+
+				    }
+				}
+    		});
+    	}
 	}
 
 	/*
 	 * checks network access of device
 	 */
-	public boolean isNetworkOn(Context context) { 
+	private boolean isNetworkOn(Context context) { 
 		ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE); 
 		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 	    return (networkInfo != null && networkInfo.isConnected());
 	}
 	
 	/*
-	 * need to add authentication with database & data encryption
+	 * verifies the input fields are filled with usable strings
 	 */
-	public boolean isDataValid() {
+	private boolean isDataValid() {
 	    boolean isEmailValid = Patterns.EMAIL_ADDRESS.matcher(getEmail()).matches();
 	    boolean isPasswordValid = !getPassword().isEmpty();
-	    //return isEmailValid && isPasswordValid;
-	    return true;
+	    return isEmailValid && isPasswordValid;
+	    //return true;
+	}
+	
+	/*
+	 * starts main activity after logging in is successful
+	 */
+	private void startMainActivity()
+	{
+		Intent i = new Intent(this, MainActivity.class);
+		startActivity(i);
 	}
 
-	public String getPassword() {
-	    return uPassword.getText().toString().trim(); // mEditPassword - EditText
+	/*
+	 * returns user password
+	 */
+	private String getPassword() {
+	    return uPassword.getText().toString().trim(); // user password - EditText
 	}
 
-	public String getEmail() {
-	    return uEmail.getText().toString().trim(); // mEditEmail - EditText
+	/*
+	 * returns user email
+	 */
+	private String getEmail() {
+	    return uEmail.getText().toString().trim(); // user email - EditText
 	}
 	
 }
--- Lexiday/src/com/android/lexiday/MainActivity.java
+++ Lexiday/src/com/android/lexiday/MainActivity.java
@@ -1,48 +1,64 @@
 package com.android.lexiday;
 
+import java.util.List;
+
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
 
 
 
 public class MainActivity extends ActionBarActivity {
 	
-	String token = null;
+	ParseUser currentUser;
+	TextView word, pronunciation, pos, definition;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);    
-        if (!isUserSignedIn()) {
-            finish();
-            loginActivity();
-            return;
-        }
-        else   
+        super.onCreate(savedInstanceState);   
+        setContentView(R.layout.activity_main);
+        setTitle("");
+        
+        /* initializes word info textviews */
+        word = (TextView) findViewById(R.id.word);
+        pronunciation = (TextView) findViewById(R.id.pronunciation);
+        pos = (TextView) findViewById(R.id.part_of_speech);
+        definition = (TextView) findViewById(R.id.definition);
+        
+        Parse.initialize(this, "EbTGZ0TfK4NhcGyX2X4nXS7rDbhPyZGGF4ZAha0M", "M6CfIKGVcYVHU1PrhQanrvuorIlyFXWJ3T92eku6");
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+        currentUser = ParseUser.getCurrentUser();
+        
+        if (currentUser == null) 
         {
-        	//setContentView(R.layout.activity_main);
-        	 WebView webview = new WebView(this);
-        	 setContentView(webview);
-        	 webview.loadUrl("http://www.lexiday.com/");
+        	returnToLoginActivity();
+        }
+        else
+        {
+        	updateWordOfDay();
         }
     }
 
-    private boolean isUserSignedIn(){
-    	Intent i = getIntent();
-    	token = i.getStringExtra("Token");
-    	if(token != null)	// change to check token validity / expiration
-    		return true;
-    	else
-    		return false;
-    }
-    
     private void loginActivity()
     {
     	Intent i = new Intent(this, LoginActivity.class);
     	i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+    	i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);	//stops the login activity from being pushed onto the stack history
     	startActivityForResult(i, 1);
     }
 
@@ -50,6 +66,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        menu.add("Logout");
         return true;
     }
 
@@ -62,7 +79,47 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        else
+        {
+        	ParseUser.logOut();
+        	returnToLoginActivity();
+        }
         return super.onOptionsItemSelected(item);
+    }
+    
+    /* 
+     * if current user has expired, closes activity and starts login activity 
+     */
+    private void returnToLoginActivity()
+    {
+        finish();
+        loginActivity();
+        return;
+    }
+    
+    /* 
+     * query parse DB for word, pronunciation, part of speech and definition 
+     * ************ NOTE: still need to determine how we are going to query
+     * 					  and determine correct word of the day, currently hard coded
+     */
+    private void updateWordOfDay()
+    {
+    	ParseQuery<ParseObject> query = ParseQuery.getQuery("Words");
+    	query.whereEqualTo("word", "definition");
+    	query.findInBackground(new FindCallback<ParseObject>() {
+    	    public void done(List<ParseObject> wordInfo, ParseException e) {
+    	        if (e == null) {
+    	            Log.d("word", "Retrieved " + wordInfo.size() + " words");
+    	        	ParseObject po = wordInfo.get(0);
+    	        	word.setText(po.getString("word"));
+    	        	pronunciation.setText(po.getString("pron"));
+    	        	pos.setText(po.getString("typeOfWord"));
+    	        	definition.setText(po.getString("definition"));        	        	
+    	        } else {
+    	            Log.d("word", "Error: " + e.getMessage());
+    	        }
+    	    }
+    	});
     }
  
 }
